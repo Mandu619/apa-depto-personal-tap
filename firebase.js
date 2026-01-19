48e2571f59ba1c638213ed9fdf2a4f3f7f337206
Mandu619 <mandujanooo619@gmail.com>
2026-01-19 10:12:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-// firebase.js (ES Modules + CDN) - Compatible con GitHub Pages
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBOOELFBd07Iy3vF3SUkgZQD6eG5Td5ZaU",
   authDomain: "apa-depto-personal-tap.firebaseapp.com",
   projectId: "apa-depto-personal-tap",
@@ -12,7 +11,6 @@ const firebaseConfig = {
   appId: "1:826908597018:web:5ac643714839cfe06d6aa3"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
